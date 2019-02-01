1e4d7b898a6805de5f683536cb439b426c0616ad
priyapatel0403 <priyapatel0403@tamu.edu>
2019-01-31 18:58:59 -0600

    folder for 2019 code

--- PriyaPatel/test_1.py
+++ PriyaPatel/test_1.py
@@ -3,9 +3,9 @@ import cv2
 
 img = cv2.imread('cloud.jpg',0)
 cv2.imshow('image',img)
-k = cv2.waitKey(0) & 0xFF
+k = cv2.waitKey(0)
 if k == 27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
 elif k == ord('s'): # wait for 's' key to save and exit
-    cv2.imwrite('blankandwhitecloud.png',img)
+    cv2.imwrite('blankandwh.png',img)
     cv2.destroyAllWindows()
