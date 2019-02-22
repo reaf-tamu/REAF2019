44680f77859598fe3aeb0244cb92903ec37d22d1
kellykrenek <43623742+kellykrenek@users.noreply.github.com>
2019-02-21 19:43:37 -0600

    Contours attempt

--- 2019Code/Image Processing/ImageProcessor.py
+++ 2019Code/Image Processing/ImageProcessor.py
@@ -12,10 +12,10 @@ while True:
         break
 
     blur = cv2.GaussianBlur(frame, (21, 21), 0)
-    #hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
     
     #not using hsv
-    hsv = blur
+    #hsv = blur
     
     #finds orange/yellow but not red
     #lower = [18, 50, 50]
@@ -39,7 +39,15 @@ while True:
     mask = cv2.inRange(hsv, lower, upper)
 
 
+    #contour attempt
     output = cv2.bitwise_and(frame, hsv, mask=mask)
+    #im_gray = cv2.imread(output, cv2.IMREAD_GRAYSCALE)
+    #(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    ret,thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    output = cv2.drawContours(gray, contours, -1, (100,100,100), 3)
+    
     cv2.imshow("output", output)
     if cv2.waitKey(1000) & 0xFF == ord('q'):
         break
